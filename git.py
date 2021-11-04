1a2153a039385de67b55a367b1d578cb4a101855
hhackk <66153810+hhackk@users.noreply.github.com>
2021-11-04 23:14:34 +0800

    Update git.py

--- git.py
+++ git.py
@@ -57,12 +57,12 @@ def getItems(url, type='', lang=''):
       except:
         print 'xx'
     
-    stars1 =  li.find('div', class_='f6 color-text-secondary mt-2').a
+    stars1 =  li.find('div', class_='f6 color-fg-muted mt-2').a
     stars = ""
     if stars1 != None:
       stars = stars1.get_text().strip()
     lang=""
-    language = li.find('div', class_='f6 color-text-secondary mt-2').find(name='span', attrs={"itemprop" :"programmingLanguage"})
+    language = li.find('div', class_='f6 color-fg-muted mt-2').find(name='span', attrs={"itemprop" :"programmingLanguage"})
     if language != None:
       lang =  language.get_text().strip()
     all_names.add(address)  
